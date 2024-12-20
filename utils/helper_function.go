bd9141de9b3e2e6647cd0951fa6a99ea09833d6e
Haiqal <Haiqal@local.com>
2024-12-20 22:51:35 +0800

    added new func for divide (test)

--- utils/helper_function.go
+++ utils/helper_function.go
@@ -1,9 +1,20 @@
 package utils
 
+import "fmt"
+
 func HelperFunction() string {
 	return "Helper function executed!"
 }
 
 func AddNumbers(num1 uint, num2 uint) uint {
 	return num1 + num2
+}
+
+func DivideNumbers(num1, num2 int) int {
+	defer func() {
+		fmt.Println(recover())
+	}()
+
+	solution := num1 / num2
+	return solution
 }
