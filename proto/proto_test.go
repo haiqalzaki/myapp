f9a40c11eb6962342e316b994f7076821bcd1b10
Haiqal <Haiqal@local.com>
2024-12-20 23:53:20 +0800

    added test for proto

--- proto/proto_test.go
+++ proto/proto_test.go
@@ -1,6 +1,8 @@
 package proto
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestProto_Struct(t *testing.T) {
 	tests := []struct {
@@ -49,5 +51,41 @@ func TestProto_Struct(t *testing.T) {
 				t.Errorf("Person_Create(%s, %d, %s) = %+v; want %+v", test.Name, test.Age, test.Email, result, test.Expected)
 			}
 		})
+	}
+}
+
+func TestWalk(t *testing.T) {
+	person := Person{
+		Name:  "John",
+		Age:   30,
+		Email: "john@example.com",
+	}
+
+	result := person.Walk()
+
+	expected := "John is walking... whilst eating ice cream!"
+
+	if result != expected {
+		t.Errorf("Walk() = %v; want %v", result, expected)
+	}
+}
+
+func TestEating(t *testing.T) {
+	// Create a Person instance
+	person := Person{
+		Name:  "John",
+		Age:   30,
+		Email: "john@example.com",
+	}
+
+	// Call eating() method
+	result := person.eating()
+
+	// Expected result
+	expected := " whilst eating ice cream!"
+
+	// Check if the result matches the expected output
+	if result != expected {
+		t.Errorf("eating() = %v; want %v", result, expected)
 	}
 }
